ae9dfffc9c2f97838a2e01606dc9cb419ee2b678
Pjotr Prins <pjotr.public01@thebird.nl>
2018-10-06 09:10:56 +0000

    sambamba: Fix generation of utils/ldc_version_info_.d
    See https://github.com/brewsci/homebrew-bio/pull/405

--- Formula/sambamba.rb
+++ Formula/sambamba.rb
@@ -4,19 +4,19 @@ class Sambamba < Formula
   homepage "https://lomereiter.github.io/sambamba"
   url "https://github.com/lomereiter/sambamba.git",
       :tag => "v0.6.8",
-      :revision => "96d1b755594f3075afac4b5f02f8d932c7c4919d"
+      :revision => "3bedb8b059b4fed88c175789baa3e868b527cb2e"
 
   depends_on "ldc" => :build
   depends_on "python" => :build
 
   def install
-    system "make", "utils/ldc_version_info_.d"
     system "make", "release"
     bin.install "bin/sambamba"
     pkgshare.install "BioD/test/data/ex1_header.bam"
   end
 
   test do
+    system "make", "check"
     assert_match "Usage", shell_output("#{bin}/sambamba --help 2>&1")
     system "#{bin}/sambamba",
       "sort", "-t2", "-n", "#{pkgshare}/ex1_header.bam",
